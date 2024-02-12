fec00396de9777a6a352b7e4897f88b400ec29f7
damianwojciechowski4 <110458027+damianwojciechowski4@users.noreply.github.com>
2024-02-12 17:40:02 +0000

    updated workflow and tf files

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+
+  backend "s3" {
+    bucket = "github-oidc-terraform-aws-tfstates-05022024"
+    key = "state/terraform.tfstate"
+    region = "eu-central-1"
+    encrypt = true
+    dynamodb_table = "myIaC-terraform-lockid"
+    
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
